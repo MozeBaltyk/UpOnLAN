32eea54dc73a355f57f1e9b7dc93ec09605baa35
MozeBaltyk <morze.baltyk@proton.me>
2025-05-23 01:33:25 +0200

    release and artefacts

---
+++ release/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+if [[ $# -ne 1 ]]; then exit 1; fi
+
+HARD_RELEASE=$1
+
+mkdir -p githubout
+
+# ipxe Artefacts
+mv menus/ipxe/* githubout/ 2> /dev/null || true
+
+# Menu Artefacts
+sed -i -e "s/set menu_version .*$/set menu_version ${HARD_RELEASE}/" menus/version.ipxe
+tar -czf menus.tar.gz menus/*
+mv menus.tar.gz ./githubout
+
+# Assets Artefacts

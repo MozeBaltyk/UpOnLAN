df0c98d4b72e15917f0872b0950978f21d8f75aa
MozeBaltyk <morze.baltyk@proton.me>
2025-05-23 03:09:11 +0200

    workflow_dispatch

--- release/release.sh
+++ release/release.sh
@@ -11,8 +11,8 @@ mkdir -p githubout
 mv menus/ipxe/* githubout/ 2> /dev/null || true
 
 # Menu Artefacts
-sed -i -e "s/set menu_version .*$/set menu_version ${HARD_RELEASE}/" menus/version.ipxe
-tar -czf menus.tar.gz menus/*
+sed -i -e "s/set menu_version .*$/set menu_version ${HARD_RELEASE}/" release/menus/version.ipxe
+tar -czf menus.tar.gz release/menus/*
 mv menus.tar.gz ./githubout
 
 # Assets Artefacts

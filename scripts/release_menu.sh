e36c8b5baa0944208824a0268386eb08fc9039d5
MozeBaltyk <morze.baltyk@proton.me>
2025-05-27 10:11:49 +0200

    release process

--- scripts/release_menu.sh
+++ scripts/release_menu.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
-if [[ $# -ne 1 ]]; then exit 1; fi
+if [[ $# -ne 1 ]]; then 
+echo "Usage: $0 <hard_release_version>"
+exit 1; 
+fi
 
 HARD_RELEASE=$1
+
+echo -e "\n### Releasing menu version ${HARD_RELEASE} ###\n"
 
 mkdir -p githubout
 
